7264e91a80d6b69955baa3ce12beed5edbca87fe
mokajima <mokajima.85z@gmail.com>
2019-01-07 16:59:33 +0900

    refactor: Move calculateWinner() into utils/helpers.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSort } from '@fortawesome/free-solid-svg-icons'
+import { calculateWinner } from './utils/helpers';
 import './index.css';
 
 function Square(props) {
@@ -171,26 +172,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return {
-        line: lines[i],
-        mark: squares[a]
-      };
-    }
-  }
-  return null;
-}
